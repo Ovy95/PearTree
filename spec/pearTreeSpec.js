b0fa963a6b37518d5c6519ae617be4fccd90b9b0
Ovy95 <jackoverton21@hotmail.com>
2021-05-10 16:25:27 +0100

    Part a compelete returns names of the 12 knights along with the Page who took all the pears off the tree

--- spec/pearTreeSpec.js
+++ spec/pearTreeSpec.js
@@ -21,6 +21,20 @@ describe("PearTree", function () {
     })
 
   })
+  describe("twelveKnights function, returns the knights names + the Page",function () {
+    
+    it("12 Knights and a Page attempt to pick all the pears off the tree",function() {
+      pearTree.groupPickers([
+      {name: "Steven1", role: "Knight"} ,{name: "Sally2", role: "Knight"} , {name: "Jack3", role: "Knight"} ,
+      {name: "Bob4", role: "Knight"} , {name: "Mark5", role: "Knight"},{name: "Jeff6", role: "Knight"} ,
+      {name: "Kev7", role: "Knight"} , {name: "Tails8", role: "Knight"} ,{name: "Peach9", role: "Knight"} ,
+      {name: "Rex10", role: "Knight"} ,{name: "Sarah11", role: "Knight"} ,{name: "Alice12", role: "Knight"},
+      {name: "Perfecto", role: "Page"}])
+
+      expect(pearTree.twelveKnights()).toEqual([ 'Steven1', 'Sally2', 'Jack3', 'Bob4', 'Mark5', 'Jeff6', 'Kev7', 'Tails8', 'Peach9', 'Rex10', 'Sarah11', 'Alice12', 'Perfecto' ])
+    })
+
+  })
 
 
 
--- src/pearTree.js
+++ src/pearTree.js
@@ -1,10 +1,16 @@
 class PearTree {
 
   pears = 12
+  successKnights = []
+  
   tree(isKnight){
 
     if(isKnight.role == "Knight"){
       this.pears--
+      this.successKnights.push(isKnight.name)
+    }
+    if(isKnight.role == "Page"){
+      this.successKnights.push(isKnight.name)
     }
   
     return this.pears
@@ -17,6 +23,13 @@ class PearTree {
     }
 
     return this.pears
+  }
+
+  twelveKnights(){
+    if(this.pears == 0){
+      return this.successKnights
+    }
+    
   }
 
 
