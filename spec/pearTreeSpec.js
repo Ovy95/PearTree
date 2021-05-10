896aa117d28b45e0c811a10e4b23a2febc639ae0
Ovy95 <jackoverton21@hotmail.com>
2021-05-10 15:45:51 +0100

    Function 2 complete updated tests

--- spec/pearTreeSpec.js
+++ spec/pearTreeSpec.js
@@ -9,15 +9,20 @@ describe("PearTree", function () {
     it("hardcoded returns value of pears",function() {
       expect(pearTree.tree({name: "Alice", role: "Software Engineer"})).toEqual(12)
     })
-    it("hardcoded returns value of pears",function() {
+    it("value of pears will be 11 as knight has picked one",function() {
       expect(pearTree.tree({name: "Jack", role: "Knight"})).toEqual(11)
     })
 
-    it("value of pears will be 11 as knight has picked one",function() {
+  })
+  describe("groupPickers function, Checks for group of pickers and returns the correct result depending on amount of knights",function () {
+    
+    it("value of pears will be 10 as there are two knights",function() {
       expect(pearTree.groupPickers([{name: "Clara", role: "Knight"} ,{name: "Steven", role: "Knight"} ,{name: "Alice", role: "Software Engineer"}])).toEqual(10)
     })
 
   })
+
+
 
 
 
